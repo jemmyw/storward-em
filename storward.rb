f3f5e36dfe12568fe13ddb03ac2d4b95f6224acf
Jeremy <jemmyw@gmail.com>
2011-05-23 22:35:42 +1200

    Put current path into laod paths

--- storward.rb
+++ storward.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
+$: << File.dirname(__FILE__)
+
 require 'rubygems'
-require 'bundler/setup'
+require 'bundler'
 require 'storward'
 load File.join(File.dirname(__FILE__), "config.rb")
 
